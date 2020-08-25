2f8ec2f8d224dd3cb338f16e7042edfc2d6040dd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-25 13:14:20 +0000

    Deploy gloriaJun/react-ts-realworld-example-app to github.com/gloriaJun/react-ts-realworld-example-app.git:gh-pages

---
+++ precache-manifest.905d4e3cc8fb03a4ddaf145509c24067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76143c0a1d100075089cd8d6900ef259",
+    "url": "/react-ts-realworld-example-app/index.html"
+  },
+  {
+    "revision": "85256d2078acaa738573",
+    "url": "/react-ts-realworld-example-app/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "25ffb0803fe2fea501ab",
+    "url": "/react-ts-realworld-example-app/static/js/2.c80c39f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ts-realworld-example-app/static/js/2.c80c39f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85256d2078acaa738573",
+    "url": "/react-ts-realworld-example-app/static/js/main.9274095e.chunk.js"
+  },
+  {
+    "revision": "29da10f08aa4a254c085",
+    "url": "/react-ts-realworld-example-app/static/js/runtime-main.e8fa864a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ts-realworld-example-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
